9d81777f6823e985a42ea87c22f2749a04965af6
AsesorTecnologico <77905882+AsesorTecnologico@users.noreply.github.com>
2026-07-02 12:48:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -787,8 +787,8 @@ const tomos = [
     { id: "tomo-i", name: "Tomo I", icon: "fas fa-book", type: "regular" },
     { id: "tomo-ii", name: "Tomo II", icon: "fas fa-book", type: "regular" },
     { id: "tomo-iii", name: "Tomo III", icon: "fas fa-book", type: "regular" },
-    { id: "tomo-iv", name: "Tomo IV", icon: "fas fa-book", type: "regular" }
-    { id: "tomo-v", name: "Tomo V", icon: "fas fa-book", type: "regular" },
+    { id: "tomo-iv", name: "Tomo IV", icon: "fas fa-book", type: "regular" },
+    { id: "tomo-v", name: "Tomo V", icon: "fas fa-book", type: "regular" }
     //{ id: "tomo-vi", name: "Tomo VI", icon: "fas fa-book", type: "regular" },
     //{ id: "intensivo-i", name: "Intensivo I", icon: "fas fa-bolt", type: "intensivo" },
     //{ id: "intensivo-ii", name: "Intensivo II", icon: "fas fa-bolt", type: "intensivo" },
